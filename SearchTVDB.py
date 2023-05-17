28a9bd1c7a373958e4a57abb25d979b7bd4f626d
Alexander Snyder <alexander@thisguyshouldworkfor.us>
2023-05-17 15:45:58 -0700

    Working

--- SearchTVDB.py
+++ SearchTVDB.py
@@ -27,24 +27,49 @@
 # Search The TV Database for show IDs and rename collection accordingly
 # --------------------------------------------------------------
 
-import tvdb_v4_official
+import requests
+import os
+import json
+import re
 
-# Get The TVDB API
-api_file_path = "./secret/tvdb.api"
-with open(api_file_path, "r") as file:
+# Get the Bearer Token
+token_file_path = "./secret/token"
+with open(token_file_path, "r") as file:
     for line in file:
-        TVDB_API = str(line.strip())
+        BEARER_TOKEN = str(line.strip())
 
-# Get The TVDB Key
-pin_file_path = "./secret/tvdb.pin"
-with open(pin_file_path, "r") as file:
-    for line in file:
-        TVDB_PIN = str(line.strip())
+BASE_URL = "https://api4.thetvdb.com/v4"
 
-print(f"TVDB Pin: {TVDB_PIN}")
-print(f"TVDB API: {TVDB_API}")
+TV_PATHS=["P:\\tv.shows", "P:\\tv.docs", "P:\\tv.kids"]
 
-tvdb = tvdb_v4_official.TVDB(TVDB_API)
+for PATH in TV_PATHS:
+    FOLDERS = os.listdir(PATH)
+    for FOLDER in FOLDERS:
+        if 'XXXXXX' in FOLDER:
+            QUERY = (FOLDER.split('(', 1)[0]).strip()
 
-# IMDB ID for TV Show '1883'
-tvdb.search_by_remote_id(remoteid='tt13991232')
+            ENDPOINT = f"/search?query='{QUERY}'"
+
+            # Form the full URL
+            url = BASE_URL + ENDPOINT
+
+            # Include the bearer token in the headers
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer {}'.format(BEARER_TOKEN)
+            }
+
+            # Make the request
+            response = requests.get(url, headers=headers)
+
+            # The response of the request can be read as json
+            response_data = response.json()
+
+            TVDB_ID = (response_data['data'][0]['tvdb_id']).strip()
+            NEW_FOLDER = re.sub('XXXXXX', TVDB_ID, FOLDER)
+
+            print(f"Old Folder: {FOLDER}\nNew Folder: {NEW_FOLDER}\n")
+            try:
+                os.rename(f'{PATH}\\{FOLDER}', f'{PATH}\\{NEW_FOLDER}')
+            except PermissionError:
+                print(f"Permission Error on: {PATH}\\{FOLDER} --> {PATH}\\{NEW_FOLDER}")
